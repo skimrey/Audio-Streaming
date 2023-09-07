07b56579938fdf6b1c2b5b431f34f354619dca9a
skimrey <89326872+skimrey@users.noreply.github.com>
2023-09-06 22:58:06 -0500

    Update api.js

--- frontend/netlify/functions/api.js
+++ frontend/netlify/functions/api.js
@@ -7,7 +7,31 @@ const api = express();
 
 const router = Router();
 router.get('/hello', (req, res) => res.send('Hello World!'));
+api.get('/audio/:id', async (req, res) => {
+  const audioId = req.params.id;
 
+  try {
+      // Fetch audio content from your PostgreSQL database based on the ID
+      const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
+
+      if (result.rows.length === 0) {
+          res.status(404).send('Audio not found');
+          return;
+      }
+
+      // Get the audio content from the database
+      const audioContent = result.rows[0].audio_content;
+
+      // Set the appropriate content type for audio
+      res.set('Content-Type', 'audio/mpeg');
+
+      // Send the audio content to the client
+      res.send(audioContent);
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+  }
+});
 api.use('/api/', router);
 
 export const handler = serverless(api);
