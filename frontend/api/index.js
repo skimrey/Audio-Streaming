a705de01cae9b8af3f52143ff13ea247f10fd1ce
skimrey <89326872+skimrey@users.noreply.github.com>
2023-09-07 00:21:16 -0500

    Update index.js

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -6,15 +6,14 @@ const pool = new Pool({
 });
 
 module.exports = async (req, res) => {
-  const audioId = req.params.id;
+  const { id } = req.query; // Vercel handles query parameters differently
 
   try {
     // Fetch audio content from your PostgreSQL database based on the ID
-    const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
+    const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [id]);
 
     if (result.rows.length === 0) {
-      res.status(404).send('Audio not found');
-      return;
+      return res.status(404).send('Audio not found');
     }
 
     // Get the audio content from the database
@@ -24,9 +23,9 @@ module.exports = async (req, res) => {
     res.setHeader('Content-Type', 'audio/mpeg');
 
     // Send the audio content to the client
-    res.send(audioContent);
+    return res.send(audioContent);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Internal Server Error');
+    return res.status(500).send('Internal Server Error');
   }
 };
