03dc85a481cd078c2d735274e91cb50e692a2ee3
skimrey <89326872+skimrey@users.noreply.github.com>
2023-09-07 00:39:46 -0500

    a

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,32 +1,11 @@
-const { Pool } = require('pg');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Create a PostgreSQL pool
-const pool = new Pool({
-  connectionString: 'postgres://bubtrxjh:zzEdCEA7lXwGx7Aexz_XlHoqzmRan2K2@bubble.db.elephantsql.com:5432/bubtrxjh',
+app.get('/vercel', (req, res) => {
+  res.json({ message: 'Hello from Vercel Express API!' });
 });
 
-module.exports = async (req, res) => {
-  
-  const { id } = req.query; // Vercel handles query parameters differently
-  console.log('Incoming request with ID:', id);
-  try {
-    // Fetch audio content from your PostgreSQL database based on the ID
-    const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [id]);
-
-    if (result.rows.length === 0) {
-      return res.status(404).send('Audio not foundddddddd');
-    }
-
-    // Get the audio content from the database
-    const audioContent = result.rows[0].audio_content;
-
-    // Set the appropriate content type for audio
-    res.setHeader('Content-Type', 'audio/mpeg');
-
-    // Send the audio content to the client
-    return res.send(audioContent);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send('Internal Server Error');
-  }
-};
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
