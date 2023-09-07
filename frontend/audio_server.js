10ca6456b4d62a6563f7b9476ae9416f5efff16a
skimrey <89326872+skimrey@users.noreply.github.com>
2023-09-06 23:43:30 -0500

    s

--- frontend/audio_server.js
+++ frontend/audio_server.js
@@ -64,28 +64,48 @@ app.get('/audio_list', async (req, res) => {
   }
 });
 
-app.get('/audio/:id', async (req, res) => {
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+import { Pool } from 'pg'; // Import Pool from 'pg'
+
+const api = express();
+const router = Router();
+
+// Create a PostgreSQL pool
+const pool = new Pool({
+  connectionString: 'postgres://bubtrxjh:zzEdCEA7lXwGx7Aexz_XlHoqzmRan2K2@bubble.db.elephantsql.com:5432/bubtrxjh',
+});
+
+// Define a route to handle audio file retrieval by ID
+router.get('/audio/:id', async (req, res) => {
   const audioId = req.params.id;
 
   try {
-      // Fetch audio content from your PostgreSQL database based on the ID
-      const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
+    // Fetch audio content from your PostgreSQL database based on the ID
+    const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
 
-      if (result.rows.length === 0) {
-          res.status(404).send('Audio not found');
-          return;
-      }
+    if (result.rows.length === 0) {
+      res.status(404).send('Audio not found');
+      return;
+    }
 
-      // Get the audio content from the database
-      const audioContent = result.rows[0].audio_content;
+    // Get the audio content from the database
+    const audioContent = result.rows[0].audio_content;
 
-      // Set the appropriate content type for audio
-      res.set('Content-Type', 'audio/mpeg');
+    // Set the appropriate content type for audio
+    res.set('Content-Type', 'audio/mpeg');
 
-      // Send the audio content to the client
-      res.send(audioContent);
+    // Send the audio content to the client
+    res.send(audioContent);
   } catch (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 });
+
+// Mount the router at the '/api/' path
+api.use('/api/', router);
+
+// Export the serverless handler
+export const handler = serverless(api);
+
--- frontend/netlify/functions/api.js
+++ frontend/netlify/functions/api.js
@@ -1,48 +1,8 @@
-const express = require("express");
-const serverless = require("serverless-http");
-
-// Create an instance of the Express app
-const app = express();
-const pool = new Pool({
-  connectionString: 'postgres://bubtrxjh:zzEdCEA7lXwGx7Aexz_XlHoqzmRan2K2@bubble.db.elephantsql.com:5432/bubtrxjh',
-});
-// Create a router to handle routes
-const router = express.Router();
-
-// Define a route that responds with a JSON object when a GET request is made to the root path
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
-});
-app.get('/audio/:id', async (req, res) => {
-  const audioId = req.params.id;
-
-  try {
-      // Fetch audio content from your PostgreSQL database based on the ID
-      const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
-
-      if (result.rows.length === 0) {
-          res.status(404).send('Audio not found');
-          return;
-      }
-
-      // Get the audio content from the database
-      const audioContent = result.rows[0].audio_content;
-
-      // Set the appropriate content type for audio
-      res.set('Content-Type', 'audio/mpeg');
-
-      // Send the audio content to the client
-      res.send(audioContent);
-  } catch (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-  }
-});
-// Use the router to handle requests to the `/.netlify/functions/api` path
-app.use(`/.netlify/functions/api`, router);
-
-// Export the app and the serverless function
-module.exports = app;
-module.exports.handler = serverless(app);
+import express, {Router} from 'express'
+import serverless from 'serverless-http'
+export async function handler(event, context) {
+  const app = express()
+  const router = Router()
+  app.use('/api/', router)
+  return serverless(app)(event, context)
+}
