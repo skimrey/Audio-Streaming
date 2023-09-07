d2a7315179f8d1de96f68f8adea9dc3704c36aa4
skimrey <89326872+skimrey@users.noreply.github.com>
2023-09-06 22:38:44 -0500

    Update api.js

--- frontend/netlify/functions/api.js
+++ frontend/netlify/functions/api.js
@@ -1,8 +1,10 @@
+import express, {Router} from 'express';
+import serverless from 'serverless-http';
 const express = require('express');
 const { Pool } = require('pg');
 const serverless = require('serverless-http');
 const cors = require('cors');
-const app = express();
+const api = express();
 const router = Router();
 
 // Create a PostgreSQL pool
@@ -34,6 +36,32 @@ app.get('/audio_list', async (req, res) => {
 });
 
 app.get('/audio/:id', async (req, res) => {
+  const audioId = req.params.id;
+
+  try {
+    // Fetch audio content from your PostgreSQL database based on the ID
+    const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
+
+    if (result.rows.length === 0) {
+      res.status(404).send('Audio not found');
+      return;
+    }
+
+    // Get the audio content from the database
+    const audioContent = result.rows[0].audio_content;
+
+    // Set the appropriate content type for audio
+    res.set('Content-Type', 'audio/mpeg');
+
+    // Send the audio content to the client
+    res.send(audioContent);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+api.get('/audio/:id', async (req, res) => {
   const audioId = req.params.id;
 
   try {
