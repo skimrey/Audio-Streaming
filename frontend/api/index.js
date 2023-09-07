43dd3a6dc5ce41d49daf2590cd90bb1a87b9a65b
skimrey <89326872+skimrey@users.noreply.github.com>
2023-09-07 00:42:46 -0500

    Update index.js

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require("express");
 
-app.get('/vercel', (req, res) => {
-  res.json({ message: 'Hello from Vercel Express API!' });
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/home", (req, res) => {
+  res.send("HOME!");
+});
+
+app.listen(5000, () => {
+  console.log("Running on port 5000!");
 });
