564e8d1fdf1887367d01c608e3050b15ce71f4a1
skimrey <89326872+skimrey@users.noreply.github.com>
2023-09-06 22:53:26 -0500

    Update api.js

--- frontend/netlify/functions/api.js
+++ frontend/netlify/functions/api.js
@@ -1,88 +1,16 @@
-const express = require('express');
-const { Pool } = require('pg');
-const serverless = require('serverless-http');
-const cors = require('cors');
+// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+
 const api = express();
+
 const router = Router();
-
-// Create a PostgreSQL pool
-const pool = new Pool({
-  connectionString: 'postgres://bubtrxjh:zzEdCEA7lXwGx7Aexz_XlHoqzmRan2K2@bubble.db.elephantsql.com:5432/bubtrxjh',
-});
-
-
-router.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // Update the path to your HTML file
-});
-
-app.get('/audio_list', async (req, res) => {
-  try {
-    // Fetch a list of audio metadata from your PostgreSQL database
-    const result = await pool.query('SELECT id, title FROM audio_metadata');
-
-    if (result.rows.length === 0) {
-      res.status(404).send('Audio not found');
-      return;
-    }
-
-    // Send the list of audio items as JSON
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-router.get('/audio/:id', async (req, res) => {
-  const audioId = req.params.id;
-
-  try {
-    // Fetch audio content from your PostgreSQL database based on the ID
-    const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
-
-    if (result.rows.length === 0) {
-      res.status(404).send('Audio not found');
-      return;
-    }
-
-    // Get the audio content from the database
-    const audioContent = result.rows[0].audio_content;
-
-    // Set the appropriate content type for audio
-    res.set('Content-Type', 'audio/mpeg');
-
-    // Send the audio content to the client
-    res.send(audioContent);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
 router.get('/hello', (req, res) => res.send('Hello World!'));
-api.get('/audio/:id', async (req, res) => {
-  const audioId = req.params.id;
 
-  try {
-    // Fetch audio content from your PostgreSQL database based on the ID
-    const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
+api.use('/api/', router);
 
-    if (result.rows.length === 0) {
-      res.status(404).send('Audio not found');
-      return;
-    }
+export const handler = serverless(api);
 
-    // Get the audio content from the database
-    const audioContent = result.rows[0].audio_content;
-
-    // Set the appropriate content type for audio
-    res.set('Content-Type', 'audio/mpeg');
-
-    // Send the audio content to the client
-    res.send(audioContent);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
 api.use('/api/', router);
 module.exports.handler = serverless(app);
