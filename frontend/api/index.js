a705d2e9ee6019181b64ea960155a9c063bde6a4
skimrey <89326872+skimrey@users.noreply.github.com>
2023-09-07 00:13:55 -0500

    Update index.js

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -6,33 +6,29 @@ const pool = new Pool({
 });
 
 module.exports = async (req, res) => {
-  const audioId = req.params.id;
-
-  try {
-    // Fetch audio content from your PostgreSQL database based on the ID
-    const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
-
-    if (result.rows.length === 0) {
-      res.status(404).send('Audio not found');
-      return;
-    }
-
-    // Get the audio content from the database
-    const audioContent = result.rows[0].audio_content;
-
-    // Set the appropriate content type for audio
-    res.setHeader('Content-Type', 'audio/mpeg');
-
-    // Create a read stream from the audio content
-    const audioStream = new Readable();
-    audioStream._read = () => {}; // Dummy implementation required for Readable streams
-    audioStream.push(audioContent);
-    audioStream.push(null); // End the stream
-
-    // Pipe the audio stream to the response object to stream the audio to the client
-    audioStream.pipe(res);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
+    app.get('/audio/:id', async (req, res) => {
+        const audioId = req.params.id;
+      
+        try {
+            // Fetch audio content from your PostgreSQL database based on the ID
+            const result = await pool.query('SELECT audio_content FROM audio_metadata WHERE id = $1', [audioId]);
+      
+            if (result.rows.length === 0) {
+                res.status(404).send('Audio not found');
+                return;
+            }
+      
+            // Get the audio content from the database
+            const audioContent = result.rows[0].audio_content;
+      
+            // Set the appropriate content type for audio
+            res.set('Content-Type', 'audio/mpeg');
+      
+            // Send the audio content to the client
+            res.send(audioContent);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        }
+      });
 };
